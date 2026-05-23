10301a0c38e01dbd4367675e7766eb311344c523
Alex Cheuk <cheuk.alex@gmail.com>
2026-05-22 17:41:49 -0700

    chore(mobile): wipe src/ for PWA port reset

--- apps/mobile/src/app/index.tsx
+++
@@ -1,8 +0,0 @@
-import { useRepos } from '@/data/context';
-import { Redirect } from 'expo-router';
-
-export default function Index() {
-  const { lifts } = useRepos();
-  const hasLifts = lifts.list().length > 0;
-  return <Redirect href={hasLifts ? '/(tabs)/home' : '/onboarding'} />;
-}
--- apps/mobile/src/data/queries/index.ts
+++
@@ -1,7 +0,0 @@
-export { queryKeys } from './keys';
-export { useActiveCycle } from './useActiveCycle';
-export { useCompleteSet } from './useCompleteSet';
-export { useHistory } from './useHistory';
-export { usePRStrip } from './usePRStrip';
-export { useSession } from './useSession';
-export { useStartSet } from './useStartSet';
